95506eedc48bcfad04e2ec1a3498618cf9726a84
Tiedapang <2750428832@qq.com>
2020-11-25 21:39:06 +0800

    [thj] feat: finished the problem 1

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Chat.scss';
 import ChatHeader from './ChatHeader/ChatHeader';
 import ChatBox from './ChatBox/ChatBox';
@@ -7,36 +7,66 @@ import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
 
 class Chat extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      shop: {},
-      messages: [],
-    };
-  }
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            shop: {},
+            messages: [],
+        };
+    }
 
-  componentDidMount() {
-    const defaultMessage = answersData.find((answer) => answer.tags.includes('DEFAULT'));
-    const messages = this.state.messages.concat(defaultMessage);
+    componentDidMount() {
+        const defaultMessage = answersData.find((answer) => answer.tags.includes('DEFAULT'));
+        const messages = this.state.messages.concat(defaultMessage);
 
-    setTimeout(() => {
-      this.setState({
-        shop: shopData,
-        messages,
-      });
-    }, 1000);
-  }
+        setTimeout(() => {
+            this.setState({
+                shop: shopData,
+                messages,
+            });
+        }, 1000);
+    }
 
-  render() {
-    const { shop, messages } = this.state;
-    return (
-      <main className="Chat">
-        <ChatHeader shop={shop} />
-        <ChatBox messages={messages} />
-        <ChatInput />
-      </main>
-    );
-  }
-}
+    handleInput = (inputMessage) => {
+        const inputMsg = {
+            role: 'CUSTOMER',
+            text: inputMessage
+        }
+        const newMsg = this.state.messages;
+        newMsg.push(inputMsg);
+        newMsg.push(this.getReplayForInput(inputMessage));
+        this.setState({
+            messages: newMsg,
+        })
+        console.info(this.state.messages);
+    }
+    getReplayForInput = (inputMessage) => {
+        const replay = answersData.find( item => inputMessage.indexOf(item.tags) > -1);
+        const defaultReplay = {
+                "text": "抱歉，现在是机器客服，暂时无法回答您的问题。",
+                "role": "ROBOT"
+            };
+        return replay === undefined? defaultReplay: replay;
+            // return {
+            //     "text": "抱歉，现在是机器客服，暂时无法回答您的问题。",
+            //     "role": "ROBOT"
+            // }
 
-export default Chat;
+    }
+
+        render()
+        {
+            const {shop, messages} = this.state;
+            return (
+                <main className="Chat">
+                    <ChatHeader shop={shop}/>
+                    <ChatBox messages={messages}/>
+                    <ChatInput handleInput={this.handleInput}/>
+                </main>
+            );
+        }
+    }
+
+    export
+    default
+    Chat;
--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
-  render() {
-    return (
-      <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
-      </footer>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            message: "",
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        if (this.state.message !== "") {
+            this.props.handleInput(this.state.message)
+            this.setState({
+                message: ""
+            })
+        }
+
+    }
+
+
+    render() {
+        return (
+            <footer className="ChatInput">
+                <input type="text" onChange={this.handleChange} value={this.state.message}/>
+                <button onClick={this.handleSubmit} type="button">Send</button>
+            </footer>
+        );
+    }
 }
 
 export default ChatInput;
